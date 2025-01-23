4cb3a607baf5b47ca658da1f20581cdfb1a7d8d0
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2025-01-23 16:26:38 +0300

    update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-'''imple Flask web application.
-'''
+'''Simple Flask web application.'''
 from flask import Flask
-
 
 app = Flask(__name__)
 '''The Flask application instance.'''
 app.url_map.strict_slashes = False
 
-
-@app.route('/')
+@app.route('/airbnb-onepage/')
 def index():
     '''The home page.'''
     return 'Hello HBNB!'
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
