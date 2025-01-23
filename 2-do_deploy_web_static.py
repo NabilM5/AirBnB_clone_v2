702c0038162f962bb69e04c223c570cc3da5a31a
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2025-01-23 16:45:50 +0300

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,39 +8,23 @@ from fabric.api import put, run, env
 from os.path import exists
 env.hosts = ['54.89.109.87', '100.25.190.21']
 
+
 def do_deploy(archive_path):
     """distributes an archive to the web servers"""
-    if not exists(archive_path):
+    if exists(archive_path) is False:
         return False
     try:
         file_n = archive_path.split("/")[-1]
         no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
-
-        # Upload the archive to the /tmp/ directory of the web server
         put(archive_path, '/tmp/')
-
-        # Uncompress the archive to the folder /data/web_static/releases/<archive filename without extension>
         run('mkdir -p {}{}/'.format(path, no_ext))
         run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-
-        # Delete the archive from the web server
         run('rm /tmp/{}'.format(file_n))
-
-        # Move the contents of the archive to the final location
         run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-
-        # Remove the now empty directory
         run('rm -rf {}{}/web_static'.format(path, no_ext))
-
-        # Delete the symbolic link
         run('rm -rf /data/web_static/current')
-
-        # Create a new symbolic link
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-
-        print("New version deployed!")
         return True
-    except Exception as e:
-        print(f"Deployment failed: {e}")
+    except:
         return False
